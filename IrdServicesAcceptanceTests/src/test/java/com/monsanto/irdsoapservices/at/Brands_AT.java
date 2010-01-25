66e9ae675db371b78676585c682590c10fc9541f
RCCLARK <rcclark@monsanto.com>
2010-01-25 01:53:35 +0000

    Changes to fix the tests since the data has changed

--- IrdServicesAcceptanceTests/src/test/java/com/monsanto/irdsoapservices/at/Brands_AT.java
+++ IrdServicesAcceptanceTests/src/test/java/com/monsanto/irdsoapservices/at/Brands_AT.java
@@ -61,7 +61,7 @@ public class Brands_AT extends AbstractAcceptanceTestCase {
         assertEquals("CORN", response.getGetBrandsResponseBody().getBrandsForSpecie().get(0).getSeedSpecieName());
         assertEquals(189, response.getGetBrandsResponseBody().getBrandsForSpecie().get(0).getBrand().size());
         assertEquals("SOYBEAN", response.getGetBrandsResponseBody().getBrandsForSpecie().get(1).getSeedSpecieName());
-        assertEquals(209, response.getGetBrandsResponseBody().getBrandsForSpecie().get(1).getBrand().size());
+        assertEquals(210, response.getGetBrandsResponseBody().getBrandsForSpecie().get(1).getBrand().size());
     }
 
     private GetBrandsRequestType createBrandsRequest(boolean isValid, String[] species) throws Exception {
