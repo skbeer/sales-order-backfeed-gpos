949a8a9815a00409ab22fa6d11e468c6c257cf6f
SVADLAM <svadlam@monsanto.com>
2010-05-03 21:26:47 +0000

    added Vistive processor list service

---
+++ IrdServicesWeb/src/main/java/com/monsanto/irdsoapservices/dao/impl/VistiveProcessorDAOImpl.java
@@ -0,0 +1,24 @@
+package com.monsanto.irdsoapservices.dao.impl;
+
+import com.monsanto.irdsoapservices.dao.VistiveProcessorDAO;
+import com.monsanto.irdsoapservices.to.ProcessorTO;
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.sql.Timestamp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SVADLAM
+ * Date: Apr 19, 2010
+ * Time: 1:03:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VistiveProcessorDAOImpl extends SqlMapClientDaoSupport implements VistiveProcessorDAO {
+
+    public List<ProcessorTO> getVistiveProcessorListFromDB() {
+        return getSqlMapClientTemplate().queryForList("processors.getVistiveProcessors");
+    }
+}
